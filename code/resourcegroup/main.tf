528b6ae639b38c392011e372e527bdc24568e2aa
pmyers1969 <pmyers1969@hotmail.com>
2023-08-30 17:49:57 +1000

    Update main.tf

--- code/resourcegroup/main.tf
+++ code/resourcegroup/main.tf
@@ -6,12 +6,12 @@ terraform {
 provider "azurerm" { 
    subscription_id = var.ARM_SUBSCRIPTION_ID
    client_id = var.ARM_CLIENT_ID
-   client_secret = ARM_CLIENT_SECRET
-   tenant_id = ARM_TENANT_ID
+   client_secret = var.ARM_CLIENT_SECRET
+   tenant_id = var.ARM_TENANT_ID
    features {}
 }
 
-resource "azurerm_resource_group" "slotDemo" {
+resource "azurerm_resource_group" "rgdemo" {
     name = var.rg_name
     location = var.location
 }
