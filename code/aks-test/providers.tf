8d914ae45daa31f17a179bc56bbb9123ce23fecb
Paul Myers <pmyers1969@hotmail.com>
2023-09-01 15:07:04 +1000

    update for aks test

---
+++ code/aks-test/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    subscription_id = var.ARM_SUBSCRIPTION_ID
+    client_id = var.ARM_CLIENT_ID
+    client_secret = var.ARM_CLIENT_SECRET
+    tenant_id = var.ARM_TENANT_ID
+  }
+}
