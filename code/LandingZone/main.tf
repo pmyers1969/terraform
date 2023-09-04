0e5071055b9dfd0038964438236d9845f88dc8cf
Paul Myers <pmyers1969@hotmail.com>
2023-09-05 08:51:03 +1000

    commit afd - first time

---
+++ code/LandingZone/main.tf
@@ -0,0 +1,30 @@
+locals {
+  tags = {
+    environment = "test"
+    department = "IT"
+    source = "terraform"
+    "created by" = "someone"
+  }
+}
+
+terraform {
+  backend "azurerm" {
+  }
+}
+# Configure the Azure provider
+provider "azurerm" { 
+   subscription_id = var.ARM_SUBSCRIPTION_ID
+   client_id = var.ARM_CLIENT_ID
+   client_secret = var.ARM_CLIENT_SECRET
+   tenant_id = var.ARM_TENANT_ID
+   features {}
+}
+
+resource "azurerm_resource_group" "rgdemo" {
+    name = var.rg_name
+    location = var.loc
+
+    tags = merge(local.tags, {
+    workload = "testresources"
+  })
+}
