f3ce8b9df3bdb79ec192af7bdb2b820062e4ad98
Paul Myers <pmyers1969@hotmail.com>
2023-09-19 16:32:03 +1000

    landing zone start

--- code/LandingZone/lz-main.tf
+++ code/LandingZone/lz-main.tf
@@ -7,19 +7,6 @@ locals {
   }
 }
 
-terraform {
-  backend "azurerm" {
-  }
-}
-# Configure the Azure provider
-provider "azurerm" { 
-   subscription_id = var.ARM_SUBSCRIPTION_ID
-   client_id = var.ARM_CLIENT_ID
-   client_secret = var.ARM_CLIENT_SECRET
-   tenant_id = var.ARM_TENANT_ID
-   features {}
-}
-
 module "resourcegroup" {
   source = "../modules/resourcegroup"
   resource_group_name          = var.resource_group_name
