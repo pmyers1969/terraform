a1b7a818ba040e4b2d8556c806edf2f3be5cc074
Paul Myers <pmyers1969@hotmail.com>
2023-09-29 14:33:02 +1000

    more changes

--- code/modules/subnet/mod-subnet.tf
+++ code/modules/subnet/mod-subnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "subnet" {
-  for_each = var.subnets
+  for_each = var.subnet
   resource_group_name       = var.resource_group_name
   virtual_network_name      = var.vnet_name
   name                      = each.value["name"]
