c9843a5a99a5089615dd412c4d8fb9089f769cc0
Paul Myers <pmyers1969@hotmail.com>
2023-09-29 13:54:50 +1000

    more changes

--- code/modules/vnet/vnet.tf
+++ code/modules/vnet/vnet.tf
@@ -1,7 +1,7 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  address_space       = var.vnet_address_space
+  address_space       = [var.vnet_address_space]
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
