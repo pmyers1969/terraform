9ee8c514d8c5af38e1d5e9fbf44ef9c500d7bcb2
Paul Myers <pmyers1969@hotmail.com>
2023-10-11 08:34:26 +1100

    updated type

--- code/modules/subnet/mod-subnet.tf
+++ code/modules/subnet/mod-subnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "subnet" {
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.vnet.name
+  virtual_network_name      = var.vnet_name
   name                      = var.subnet_name
   address_prefixes          = var.address_prefix
 }
