1419b54ab02790f39dab7472da89a15cf100cbd9
Paul Myers <pmyers1969@hotmail.com>
2023-10-10 17:49:28 +1100

    updated type

--- code/LandingZone/lz-variables.tf
+++ code/LandingZone/lz-variables.tf
@@ -48,32 +48,3 @@ variable "vnets" {
 
 
 
-#variable "vnet_name" {
-#  type        = string
-#  description = "Virtual Network Name"
-#  default     = "default-vnet"
-#}
-
-#variable "vnet_address_space" {
-#  type        = string
-#  description = "Virtual Network Address Space"
-#  default     = "10.0.0.0/16"
-#}
-
-#variable "subnets" {
-#  type = map(any)
-#  default = {
-#    subnet_1 = {
-#      name             = "subnet_1"
-#      address_prefixes = ["10.0.1.0/24"]
-#    }
-#    subnet_2 = {
-#      name             = "subnet_2"
-#      address_prefixes = ["10.0.2.0/24"]
-#    }
-#    subnet_3 = {
-#      name             = "subnet_3"
-#      address_prefixes = ["10.0.3.0/24"]
-#    }
-#  }
-#}
