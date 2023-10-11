3e8d9b5bf49e088bf3216c1f35e9965aacef0182
Paul Myers <pmyers1969@hotmail.com>
2023-10-12 10:23:53 +1100

    stuff

--- code/stuff/variables.tf
+++ code/stuff/variables.tf
@@ -34,3 +34,19 @@ variable "vnets" {
     ],
   }
 }
+
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+}
