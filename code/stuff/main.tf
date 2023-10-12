2311c0190d5dfcd9f3e6e474fec6cd7388f3f54b
Paul Myers <pmyers1969@hotmail.com>
2023-10-12 13:27:52 +1100

    stuff

--- code/stuff/main.tf
+++ code/stuff/main.tf
@@ -24,15 +24,15 @@ resource "azurerm_resource_group" "network" {
 resource "azurerm_virtual_network" "vnets" {
   for_each            = var.vnets
   name                = each.key
-  resource_group_name = data.azurerm_resource_group.network.name
-  location            = data.azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+  location            = azurerm_resource_group.network.location
   address_space       = [each.value.address_space]
 }
 
 resource "azurerm_subnet" "subnets" {
   for_each             = local.subnets
   name                 = each.value.subnet_name
-  resource_group_name  = data.azurerm_resource_group.network.name
+  resource_group_name  = azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.vnets[each.value.vnet_name].name
   address_prefixes     = [each.value.subnet_address]
   service_endpoints    = each.value.service_endpoints
