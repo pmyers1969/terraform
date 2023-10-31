909ef0b4d4d89a907310a947e62925095c4ba07c
Paul Myers <pmyers1969@hotmail.com>
2023-10-31 16:15:13 +1100

    more changes

---
+++ code/stuff2/main.tf
@@ -0,0 +1,62 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
+  tenant_id = var.ARM_TENANT_ID
+  features {}
+}
+
+resource "azurerm_resource_group" "network" {
+  name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnets" {
+  for_each            = var.vnets
+  name                = each.key
+  resource_group_name = azurerm_resource_group.network.name
+  location            = azurerm_resource_group.network.location
+  address_space       = each.value.cidr_block
+}
+
+locals {
+  network_subnets = flatten([
+    for network_key, network in var.vnets : [
+      for subnet_key, subnet in network.subnets : {
+        network_key   = network_key
+        subnet_key    = subnet_key
+        subnet_name   = subnet.subnet_name
+        cidr_block    = subnet.cidr_block
+      }
+    ]
+  ])
+//  subnets = { for subnet in local.subnets_flatlist : subnet.subnet_name => subnet ...}
+}
+
+resource "azurerm_subnet" "subnets" {
+  for_each             = { 
+    for subnet in local.network_subnets : subnet.subnet_name => subnet ...
+  }
+
+  name                 = each.value.subnet_name
+  resource_group_name  = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnets[each.value.vnet_name].name
+  address_prefixes     = [each.value.cidr_block]
+}
+
+
+
+output "result" {
+  value = [for subnets in azurerm_subnet.subnets: subnets.name]
+}
+
---
+++ code/stuff2/variables.tf
@@ -0,0 +1,76 @@
+variable "resource_group_name" {
+  description = "Name of the resource group to be imported."
+  type        = string
+  default     = "rg-testagain"
+}
+
+variable "location" {
+  description = "The location of the vnet to create. Defaults to the location of the resource group."
+  type        = string
+  default     = "AustraliaSoutheast"
+}
+
+variable "vnets" {
+  type = map(object({
+    cidr_block = string
+    subnets = map(object({
+      cidr_block = string 
+    }))
+  }))
+  default = {
+    "bupavnet1"             = {
+      cidr_block            = "192.168.0.0/16"
+      subnets               = {
+        "subnet1_bupavnet1" = {
+           subnet_name      = "subnet1_bupavnet1"
+           cidr_block       = "192.168.0.0/24"
+        }
+      }
+    },
+    "bupavnet2"             = {
+      cidr_block            = "10.0.0.0/16"
+      subnets               = {
+        "subnet1_bupavnet2" = {
+           subnet_name      = "subnet1_bupavnet2"
+           cidr_block       = "10.0.2.0/24"
+        }
+      }
+    },
+    "bupavnet3"             = {
+      cidr_block            = "10.80.0.0/16"
+      subnets               = {
+        "subnet1_bupavnet3" = {
+           subnet_name      = "subnet1_bupavnet3"
+           cidr_block       = "10.80.2.0/24"
+        },
+        "subnet2_bupavnet3" = {
+           subnet_name      = "subnet1_bupavnet3"
+           cidr_block       = "10.80.1.0/24"
+        },
+        "GatewaySubnet"     = {
+           subnet_name      = "GatewaySubnet"
+           cidr_block       = "10.80.0.0/24"
+        }
+      }
+    }
+  }
+}
+
+
+
+
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+}
