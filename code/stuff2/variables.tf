3a77798445a2432e648647826eeaa9ea62b9b927
Paul Myers <pmyers1969@hotmail.com>
2023-11-01 11:55:53 +1100

    more changes

--- code/stuff2/variables.tf
+++ code/stuff2/variables.tf
@@ -12,7 +12,7 @@ variable "location" {
 
 variable "vnets" {
   type = map(object({
-    address_space = string
+    address_space = list(string)
     subnets = map(object({
       subnet_name = string
       cidr_block = string 
@@ -20,50 +20,50 @@ variable "vnets" {
   }))
   default = {
     "bupavnet1"               = {
-      address_space         = "192.168.0.0/16"
+      address_space         = ["192.168.0.0/16"]
       subnets               = {
         "subnet1_bupavnet1" = {
            subnet_name      = "subnet1_bupavnet1"
-           cidr_block       = "192.168.0.0/24"
+           cidr_block       = ["192.168.0.0/24"]
         }
       }
     },
     "bupavnet2"               = {
-      address_space         = "10.0.0.0/16"
+      address_space         = ["10.0.0.0/16"]
       subnets               = {
         "subnet1_bupavnet2"   = {
            subnet_name      = "subnet1_bupavnet2"
-           cidr_block       = "10.0.2.0/24"
+           cidr_block       = ["10.0.2.0/24"]
         }
         "subnet2_bupavnet2"   = {
            subnet_name      = "subnet2_bupavnet2"
-           cidr_block       = "10.0.3.0/24"
+           cidr_block       = ["10.0.3.0/24"]
         }
       }
     },
     "bupavnet3"               = {
-      address_space         = "10.80.0.0/16"
+      address_space         = ["10.80.0.0/16"]
       subnets               = {
         "subnet1_bupavnet3"   = {
            subnet_name      = "subnet1_bupavnet3"
-           cidr_block       = "10.80.2.0/24"
+           cidr_block       = ["10.80.2.0/24"]
         },
         "subnet2_bupavnet3"   = {
            subnet_name      = "subnet2_bupavnet3"
-           cidr_block       = "10.80.1.0/24"
+           cidr_block       = ["10.80.1.0/24"]
         },
         "GatewaySubnet"       = {
            subnet_name      = "GatewaySubnet"
-           cidr_block       = "10.80.0.0/24"
+           cidr_block       = ["10.80.0.0/24"]
         }
       }
     },
     "bupavnet4"               = {
-      address_space         = "10.81.0.0/16"
+      address_space         = ["10.81.0.0/16"]
       subnets               = {
         "subnet1_bupavnet4" = {
            subnet_name      = "subnet1_bupavnet4"
-           cidr_block       = "10.81.0.0/24"
+           cidr_block       = ["10.81.0.0/24"]
         }
       }
     }
