3679fc664fb7b6de031e5b528d904229448de414
Paul Myers <pmyers1969@hotmail.com>
2023-11-02 13:22:47 +1100

    stuff3

---
+++ code/stuff3/main.tf
@@ -0,0 +1,33 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
+  tenant_id = var.ARM_TENANT_ID
+  features {}
+}
+
+resource "azurerm_resource_group" "network" {
+  name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnets" {
+  for_each = { for idx, server in var.vnets : idx => server }
+  name                = each.value.vnet_name
+  resource_group_name = azurerm_resource_group.network.name
+  location            = azurerm_resource_group.network.location
+  address_space       = each.value.address_space
+}
+
+
+
---
+++ code/stuff3/variables.tf
@@ -0,0 +1,44 @@
+variable "resource_group_name" {
+  description = "Name of the resource group to be imported."
+  type        = string
+  default     = "rg-testagain2"
+}
+
+variable "location" {
+  description = "The location of the vnet to create. Defaults to the location of the resource group."
+  type        = string
+  default     = "AustraliaSoutheast"
+}
+
+variable "vnets" {
+  description = "list of all networks and subnets"
+  type = list(object({
+    vnet_name = string
+    address_space = list(string)
+    }))
+  default = [{
+    vnet_name = "vnet100"
+    address_space = [ "10.20.0.0/16" ]
+   
+  }]
+}
+
+
+
+
+
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+}
