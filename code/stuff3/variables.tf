fbb39482b2f0ba2e03dfe617dbeab552a79c923d
Paul Myers <pmyers1969@hotmail.com>
2023-11-02 15:09:51 +1100

    blah

--- code/stuff3/variables.tf
+++ code/stuff3/variables.tf
@@ -15,10 +15,11 @@ variable "vnets" {
   type = list(object({
     vnet_name = string
     address_space = list(string)
-    subnets = map(object)
+    subnets = map(object({
       subnet_name = string
       cidr_block  = string
     }))
+  }))
   default = [{
     vnet_name = "vnet100"
     address_space = [ "10.20.0.0/16" ]
